6b288eaa7283c1e9a002cd52fd1b1400358173be
HomelessSkin <105777886+HomelessSkin@users.noreply.github.com>
2025-11-10 17:56:28 +0500

    27

--- Scripts/Runtime/UIElement.cs
+++ Scripts/Runtime/UIElement.cs
@@ -13,10 +13,14 @@ namespace UI
         public enum Type
         {
             Null = 0,
-            Button = 1,
+            Menu_Button = 1,
             Field_Dark = 2,
             Field_Light = 3,
-            Twitch = 4,
+            Chat_Message = 4,
+            Drop_Down_Content = 5,
+            Drop_Down_Item = 6,
+            Input_Area = 7,
+            Text = 8,
 
         }
     }
